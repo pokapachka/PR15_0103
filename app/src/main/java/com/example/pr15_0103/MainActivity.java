bd3892e3b23b49c7ff291b6e532c1aa415628b57
Pavel Osokin <pachkaosokin@yandex.ru>
2024-11-19 23:19:01 +0500

    Создание метода playStart. ОСокин

--- app/src/main/java/com/example/pr15_0103/MainActivity.java
+++ app/src/main/java/com/example/pr15_0103/MainActivity.java
@@ -62,4 +62,16 @@ public class MainActivity extends AppCompatActivity {
             mediaPlayer = null;
         }
     }
+    public void playStart(View v){
+        try{
+            releasePlayer();
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(fileName);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
