bb5500d7edb8d9c6f3d749fa093d173e31d1f486
Pavel Osokin <pachkaosokin@yandex.ru>
2024-11-19 23:15:14 +0500

    Создание метода recordStart. Осокин

--- app/src/main/java/com/example/pr15_0103/MainActivity.java
+++ app/src/main/java/com/example/pr15_0103/MainActivity.java
@@ -4,12 +4,15 @@ import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +31,24 @@ public class MainActivity extends AppCompatActivity {
         if(mediaRecorder != null){
             mediaRecorder.release();
             mediaRecorder = null;
+        }
+    }
+    public void recordStart(View v) {
+        try {
+            releaseRecorder();
+            File outFile = new File(fileName);
+            if (outFile.exists()) {
+                outFile.delete();
+            }
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setOutputFile(fileName);
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
